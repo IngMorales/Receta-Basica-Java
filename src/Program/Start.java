96c37ce1184ecc44c6a2314d3a90c30e8da3abc1
Ing. Aldair Morales Cuellar <al-dair12@hotmail.es>
2019-03-03 16:37:15 -0500

    upload
    upload

---
+++ src/Program/Start.java
@@ -0,0 +1,10 @@
+package Program;
+public class Start {
+
+    public static void main(String[] args) {
+        Window ventana = new Window();
+        ventana.setSize(400, 400);
+        ventana.setVisible(true);
+    }
+    
+}
